c7c59eeaa04e4808ce3afa08294bcb7400bf7ce4
Montywilliams <3650@holbertonschool.com>
2022-11-14 05:31:51 -0600

    import lodash

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,6 +5,23 @@ const {
     GraphQLSchema
 } = require('graphql');
 
+const _ = require('lodash');
+
+const tasks = [
+    {
+        id: 1,
+        title: 'Create your first webpage',
+        weight: 1,
+        description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+    },
+    {
+        id: 2,
+        title: 'Structure your webpage',
+        weight: 1,
+        description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'
+    }
+]
+
 const TaskType = new GraphQLObjectType({
     name: 'Task',
     fields: () => ({
@@ -14,3 +31,22 @@ const TaskType = new GraphQLObjectType({
         description: { type: GraphQLString }
       })
 })
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      task: {
+        type: TaskType,
+        args: { id: { type: GraphQLString } },
+        resolve(parent, args) {
+          
+        }
+      }
+    }
+  });
+
+const schema = new GraphQLSchema({
+    query: RootQuery
+  });
+  
+module.exports = schema;
